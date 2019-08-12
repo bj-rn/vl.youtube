774a64fa145371144e72db447bbbbdf098df2892
bj-rn <mail@plain-vanilla.net>
2019-08-12 14:17:25 +0200

    init

---
+++ VS/Youtube.Helper/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using Google.Apis.Requests;
+using Google.Apis.Upload;
+using Google.Apis.YouTube.v3.Data;
+using System.Reactive.Linq;
+using Google.Apis.YouTube.v3;
+
+namespace YouTube.Helper
+{
+    public static class YTUploadExtensions
+    {
+
+
+        public static ResumableUpload AddProgressEventHandler <T> (this ResumableUpload<T> upload, Action<IUploadProgress> callback)
+        {
+            upload.ProgressChanged += callback;
+
+            return upload;
+
+        }
+
+        public static ResumableUpload RemoveProgressEventHandler <T> (this ResumableUpload<T> upload, Action<IUploadProgress> callback)
+        {
+            upload.ProgressChanged -= callback;
+
+            return upload;
+        }
+
+
+        public static ResumableUpload AddResponseEventHandler <T> (this ResumableUpload<T, T> upload, Action<T> callback)
+        {
+            upload.ResponseReceived += callback;
+
+            return upload;
+
+        }
+
+        public static ResumableUpload RemoveResponseEventHandler <T> (this ResumableUpload<T, T> upload, Action<T> callback)
+        {
+            upload.ResponseReceived -= callback;
+
+            return upload;
+        }
+
+    }
+}
